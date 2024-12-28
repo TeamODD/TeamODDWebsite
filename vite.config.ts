601f7e8a98bc18d21f585b2309723b82b21c6493
N3RO <jho20455744@gmail.com>
2024-12-28 23:45:37 +0900

    Adds Bootstrap and path dependencies, updates @types/node version
    Integrates Bootstrap library for enhanced UI components
    Adds path dependency for file path manipulation
    Updates @types/node to version 22.10.2 for improved TypeScript support
    
    Relates to #123

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: resolve('C:/Users/gksdn/Desktop/TeamODD_Project/teamodd.github.io'),
+    emptyOutDir: true,
+  },
 })
