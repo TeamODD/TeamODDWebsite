7ffe3a5fbeb9a341b13d98cbe4cd1591c1218128
N3RO <jho20455744@gmail.com>
2025-01-24 22:22:05 +0900

    suspense 보완

--- src/App.tsx
+++ src/App.tsx
@@ -16,40 +16,11 @@ const Login = lazy(() => import("./routes/Login"));
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading />}>
-        <AppContent />
-      </Suspense>
+      <AppContent />
     </BrowserRouter>
   );
 }
 
-/**
- * `AppContent` 컴포넌트는 애플리케이션의 주요 레이아웃을 구성합니다.
- *
- * 이 컴포넌트는 `memo`를 사용하여 불필요한 리렌더링을 방지합니다.
- *
- * @returns {JSX.Element} 애플리케이션의 주요 레이아웃을 포함하는 JSX 요소를 반환합니다.
- *
- * @component
- * @example
- * ```tsx
- * <AppContent />
- * ```
- *
- * @remarks
- * 이 컴포넌트는 다음과 같은 구조로 이루어져 있습니다:
- * - `Header` 컴포넌트가 포함된 `header` 요소
- * - 여러 `Route` 컴포넌트를 포함한 `main` 요소
- * - `Footer` 컴포넌트가 포함된 `footer` 요소
- *
- * `Route` 컴포넌트는 다음과 같은 경로와 요소를 매핑합니다:
- * - `/` 경로는 `Home` 컴포넌트를 렌더링합니다.
- * - `/projects` 경로는 `Projects` 컴포넌트를 렌더링합니다.
- * - `/join` 경로는 `Join` 컴포넌트를 렌더링합니다.
- * - `/contact` 경로는 `Contact` 컴포넌트를 렌더링합니다.
- * - `/login` 경로는 `Login` 컴포넌트를 렌더링합니다.
- * - `/admin` 경로는 `ProtectedRoute` 컴포넌트를 통해 보호된 `Admin` 컴포넌트를 렌더링합니다.
- */
 const AppContent = memo(() => {
   return (
     <div className="App">
@@ -57,21 +28,23 @@ const AppContent = memo(() => {
         <Header />
       </header>
       <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute>
-                <Admin />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/admin"
+              element={
+                <ProtectedRoute>
+                  <Admin />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
       </main>
       <footer>
         <Footer />
