ffddcee9277ed93c2b29bc014a3b95c32311e506
N3RO <jho20455744@gmail.com>
2025-01-25 04:15:52 +0900

    Header 디자인 변경

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,18 @@
+import { MoonLoader } from "react-spinners";
+
 const Loading = () => {
   const style = {
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     height: "100vh",
-    fontSize: "1.5rem",
   };
 
-  return <div style={style}>로딩 중...</div>;
+  return (
+    <div style={style}>
+      <MoonLoader color="#fff" />
+    </div>
+  );
 };
 
 export default Loading;
