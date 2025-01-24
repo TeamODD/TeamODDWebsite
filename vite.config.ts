503ecef16361887c83573994fe2965ebd1664303
N3RO <jho20455744@gmail.com>
2025-01-25 05:06:17 +0900

    최적화

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({
+      // 압축 알고리즘 지정, 기본적으로는 'gzip'을 사용
+      algorithm: "gzip",
+      // 압축된 파일의 확장자를 '.gz'로 설정
+      ext: ".gz",
+    }),
+  ],
   build: {
     outDir: "dist",
     assetsDir: "assets",
     rollupOptions: {
       output: {
-        manualChunks: undefined,
+        manualChunks: {
+          react: ["react", "react-dom", "zustand", "@tanstack/react-query"],
+          reactRouter: ["react-router-dom"],
+          animations: ["framer-motion", "gsap", "aos"],
+          swiperBundle: ["swiper"],
+        },
       },
     },
   },
