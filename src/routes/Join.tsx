7f9fb03cc2f6a22bf0d1b250ea22fceebec0859a
herenero <jho20455744@gmail.com>
2025-04-02 23:03:55 +0900

    모집완료

--- src/routes/Join.tsx
+++ src/routes/Join.tsx
@@ -28,7 +28,7 @@ const Join = () => {
     <div className="join-container">
       <div className="join-content">
         <h2 className="join-title">모집 일정</h2>
-        <p className="join-text">2025-1 신입 부원 모집 중입니다!</p>
+        <p className="join-text">2025-1 모집 완료했습니다! 25-2 모집을 기다려 주세요!</p>
         <div className="join-button">
           <GradientButton
             innerText="지원하기"
